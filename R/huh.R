66dbe2606cf09c3ed5b8c804d9dacfd4a8068983
SigurdJanson <emailjs@web.de>
2022-08-12 22:04:35 +0200

    Added factory function to create huh objects

--- R/huh.R
+++ R/huh.R
@@ -1,5 +1,6 @@
 
 
+
 #' Determine the OOP paradigm of an object.
 #'
 #' @param x An object
@@ -15,6 +16,43 @@
   else if (length(xClass) > 0) "S3 class"
   else "unknown"
 }
+
+
+
+#' new_huh
+#'
+#' Factory to create `huh` objects (S3).
+#'
+#' @param .name Name of the object
+#' @param .type See `typeof()`
+#' @param .class See `class()`
+#' @param .mode See `mode()`
+#' @param .dims See `dims()`
+#' @param .paradigm The OOP the object is based on
+#'
+#' @return An `huh` object
+#' @keywords internal
+#' @noRd
+new_huh <- function(.name, .type, .class, .mode, .dims, .paradigm) {
+  if (missing(.name) || !is.character(.name))
+    stop("Object specification ('huh') needs an object name")
+  if (missing(.type) || !is.character(.type))
+    stop("Object specification ('huh') needs a type")
+
+  result <- structure(
+    list(
+      name = .name,
+      type = .type,
+      class = .class,
+      mode = .mode,
+      dimensions = .dims,
+      paradigm = .paradigm
+    ),
+    class = "huh"
+  )
+  return(result)
+}
+
 
 
 
@@ -43,16 +81,15 @@ huh <- function(x) {
   xClass <- class(x)
   attributes(xClass) <- NULL
 
-  y <- list(
-    name = deparse(substitute(x)),
-    type = typeof(x),
-    class = xClass,
-    mode = mode(x),
-    dimensions = dims,
-    paradigm = .paradigm(x)
+  return(
+    new_huh(
+      .name = deparse(substitute(x)),
+      .type = typeof(x),
+      .class = xClass,
+      .mode = mode(x),
+      .dims = dims,
+      .paradigm = .paradigm(x)
+    )
   )
-  class(y) <- "huh"
-
-  return(y)
 }
 
