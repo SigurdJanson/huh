243359e829c5e91d1cc90ca6353b2afcb340e17c
SigurdJanson <emailjs@web.de>
2022-08-15 22:38:25 +0200

    Added tests for .isS3Generic()

---
+++ tests/testthat/test-isS3Generic.R
@@ -0,0 +1,69 @@
+test_that("primitive generics detected", {
+  expect_true(.isS3Generic("["))
+  expect_true(.isS3Generic("[["))
+  #expect_true(is_s3_method("[.data.frame"))
+
+  expect_true(.isS3Generic("sum"))
+  expect_true(.isS3Generic("mean"))
+  #expect_true(is_s3_method("mean.default"))
+
+  expect_true(.isS3Generic("c"))
+  #expect_true(is_s3_method("c.Date"))
+})
+
+test_that("non-generic primitive is ruled out", {
+  expect_false(.isS3Generic("UseMethod"))
+})
+
+test_that("non-functions are not generics", {
+  a <- TRUE
+  b <- NULL
+  c <- data.frame()
+
+  expect_false(.isS3Generic("a"))
+  expect_false(.isS3Generic("b"))
+  expect_false(.isS3Generic("c"))
+})
+
+test_that("user defined generics & methods detected", {
+  my_method <- function(x) UseMethod("mymethod")
+  my_method.character <- function(x) x
+
+  expect_true(.isS3Generic("my_method"))
+  #expect_true(is_s3_method("my_method.character"))
+})
+
+test_that("methods for group generics detected", {
+  Ops.myclass <- function(x) x
+
+  expect_false(.isS3Generic("Ops.myclass"))
+  #expect_true(is_s3_method("Ops.myclass"))
+})
+
+test_that("user defined generics detected even if use non-standard", {
+  my_method <- function(x) {
+    x <- 1
+    if (x > 2) UseMethod("mymethod")
+  }
+
+  expect_true(.isS3Generic("my_method"))
+})
+
+# Test taken from roxygen2 - but why shouldn't we be able to overwrite/conceal a primitive
+# test_that("user defined functions override primitives", {
+#   c <- function(x) x + 1
+#   c.test <- function(x) x + 3
+#
+#   expect_false(.isS3Generic("c"))
+#   #expect_false(is_s3_method("c"))
+# })
+
+test_that("@method overrides auto-detection", {
+  out <- roxygen2::parse_text("
+    #' @export
+    #' @method all.equal data.frame
+    all.equal.data.frame <- function(...) 1
+  ")[[1]]
+
+  expect_equal(attr(out$object$value, "s3method"), c("all.equal", "data.frame"))
+})
