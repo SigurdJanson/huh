ddf6546453dd4a88b716186b70917759bcd18a8d
SigurdJanson <emailjs@web.de>
2022-09-18 14:24:16 +0200

    Added function to determine the nesting depth of objects

--- R/listtype.R
+++ R/listtype.R
@@ -21,3 +21,34 @@ is.nested <- function(x) {
 }
 
 
+
+
+#' depth
+#'
+#' Determines the (maximum) depth of nesting of vectors.
+#'
+#' @param x An object
+#' @param current Starting level for the nesting. Default is 0 and there
+#' is no reason to change that. If you set `current` to a value `x` the result will
+#' be equal to `depth(...) + x`.
+#'
+#' @return The maximum nesting depth found anywhere in the list.
+#' The result will (obviously) always be 1 for atomic vectors. Returns `NA` for
+#' objects that are not vectors.
+#' @export
+#'
+#' @examples
+#' depth(list(1, 2, list(3, "a"), 4, 5))
+#' depth(expression(u, 2, u + 0:9))
+depth <- function(x, current = 0L) {
+  if (is.recursive(x)) {
+    current <- current + 1L
+    result <- sapply(x, \(x) { if (is.recursive(x)) depth(x, current) else current })
+    return(max(result))
+  }
+  if (is.vector(x)) { # vector but not recursive can NOT have a depth > 1
+    return(current + 1L)
+  }
+  return(NA)
+}
+
--- tests/testthat/test-listtype.R
+++ tests/testthat/test-listtype.R
@@ -23,3 +23,38 @@ test_that("`is.nested` can handle 'falsy' objects", {
   expect_false( is.nested(Inf) )
   expect_false( is.nested(NA) )
 })
+
+
+
+
+# `depth()` ==============
+
+test_that("`depth()` gives 1 for atomic vectors", {
+  expect_identical( depth(1:5), 1L)
+  expect_identical( depth(letters), 1L)
+})
+
+test_that("`depth()` returns correct results for different kinds of lists", {
+  expect_identical( depth(list(1, 2, 3, 4, 5)), 1L)
+  expect_identical( depth(list(1, 2, 2:3, 4, 5)), 1L)
+  expect_identical( depth(list(1:2, 2:3, 3:4, letters, LETTERS)), 1L)
+
+  expect_identical( depth(list(1, 2, list(3, "a"), 4, 5)), 2L)
+  expect_identical( depth(list(list(1), list(2), list(3), list(4), list(5))), 2L)
+  expect_identical(
+    depth(list(list(1), list(2), list(3), list(list(4, list(4.1, 4.2, list(4.11))), list(letters)), list(5))),
+    5L)
+})
+
+test_that("`depth()` works for expressions", {
+  expect_identical( depth(expression(u)), 1L )
+  expect_identical( depth(expression(u, 2)), 1L )
+  expect_identical( depth(expression(u, 2, u + 0)), 2L )
+  expect_identical( depth(expression(u, 2, u + 0:9)), 3L )
+  expect_identical( depth(expression(u, 2, expression(u + 0:9))), 4L )
+})
+
+
+test_that("`depth()` returns NA for objects that aren't vectors", {
+  expect_identical( depth(NULL), NA )
+})
